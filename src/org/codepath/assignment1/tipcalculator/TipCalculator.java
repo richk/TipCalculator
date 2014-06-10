47f43fd005ba10fb5fd92f773406876e9dcbdf22
Richa Khandelwal <kricha@lab126.com>
2014-06-09 22:44:28 -0700

    Added AutoCompleteTextView. Changed font styles for all textviews.

--- src/org/codepath/assignment1/tipcalculator/TipCalculator.java
+++ src/org/codepath/assignment1/tipcalculator/TipCalculator.java
@@ -1,5 +1,17 @@
 package org.codepath.assignment1.tipcalculator;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
@@ -9,6 +21,8 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -18,14 +32,18 @@ import android.widget.TextView.OnEditorActionListener;
 public class TipCalculator extends Activity {
 	private static final String LOG_TAG = TipCalculator.class.getSimpleName();
 	private static final int DEFAULT_NUM_PEOPLE = 1;
+	private static final String ACCustomTipPercentFile = "acCustomTipPercent";
 	
 	private EditText etBaseAmount;
 	private RadioGroup rgTipPercent;
 	private TextView tvTipAmount;
 	private TextView tvBaseAmountHelper;
-	private EditText etCustomTipPercent;
+	private AutoCompleteTextView acCustomTipPercent;
 	private EditText etNumPeople;
 	private TextView tvTipPerPerson;
+	private List<String> acCustomTipPercentItems;
+	private Set<String> acCustomTipPercentSet;
+	private ArrayAdapter<String> acCustomTipPercentAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,17 +56,27 @@ public class TipCalculator extends Activity {
 	            getTipPercentAndDisplayTip();
 	        }
 	    });
-		etCustomTipPercent = (EditText) findViewById(R.id.etCustomPercent);
-		etCustomTipPercent.setOnEditorActionListener(new OnEditorActionListener() {
+		acCustomTipPercent = (AutoCompleteTextView) findViewById(R.id.autoCustomPercent);
+		acCustomTipPercentItems = new ArrayList<String>();
+		acCustomTipPercentSet = new HashSet<String>();
+		loadACItemsFromFile();
+		acCustomTipPercentAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, acCustomTipPercentItems);
+		acCustomTipPercent.setAdapter(acCustomTipPercentAdapter);
+		acCustomTipPercent.setOnEditorActionListener(new OnEditorActionListener() {
 			@Override
 			public boolean onEditorAction(TextView v, int actionId,
 					KeyEvent event) {
 				Log.d(LOG_TAG, "onEditorAction. Custom Percent");
-				hideSoftKeyboard(etCustomTipPercent);
+				hideSoftKeyboard(acCustomTipPercent);
 				int selectedTipPercentId = rgTipPercent.getCheckedRadioButtonId();
 				RadioButton selectedRb = (RadioButton) findViewById(selectedTipPercentId);
 				if (selectedRb != null && "other".equals(selectedRb.getText())) {
-					String tipPercentString = etCustomTipPercent.getText().toString();
+					String tipPercentString = acCustomTipPercent.getText().toString();
+					if (!acCustomTipPercentSet.contains(tipPercentString)) {
+						acCustomTipPercentItems.add(tipPercentString);
+						acCustomTipPercentSet.add(tipPercentString);
+						acCustomTipPercentAdapter.notifyDataSetChanged();
+					}
 					try {
 					    double tipPercent = Double.parseDouble(tipPercentString)/100;
 					    if (tipPercent <= 0) {
@@ -109,7 +137,7 @@ public class TipCalculator extends Activity {
 			String rbText = selectedRb.getText().toString();
 			String tipPercentString = "0";
 			if ("other".equals(rbText)) {
-				tipPercentString = etCustomTipPercent.getText().toString();
+				tipPercentString = acCustomTipPercent.getText().toString();
 				if (tipPercentString != null && !tipPercentString.isEmpty()) {
 					try {
 						tipPercent = Double.parseDouble(tipPercentString)/100;
@@ -192,5 +220,77 @@ public class TipCalculator extends Activity {
 	public void hideSoftKeyboard(View view){
 		  InputMethodManager imm =(InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
 		  imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+	
+	public void saveACItemsToFile() {
+		if (acCustomTipPercentSet != null && !acCustomTipPercentSet.isEmpty()) {
+			BufferedWriter writer = null;
+		    try {
+				writer = new BufferedWriter(new FileWriter(new File(getFilesDir(), ACCustomTipPercentFile)));
+				Log.d(LOG_TAG, "Saving tip percent to file");
+				for (String item : acCustomTipPercentSet) {
+					Log.d(LOG_TAG, "Item:" + item);
+					writer.write(item);
+					writer.newLine();
+				}
+			} catch (FileNotFoundException e) {
+				Log.e(LOG_TAG, "File does not exist:" + ACCustomTipPercentFile + " in parent path " + getFilesDir());
+				return;
+			} catch (IOException e) {
+				Log.e(LOG_TAG, "IOException while writing to file", e);
+				return;
+			} finally {
+			    if (writer != null) {
+			    	try {
+			    		writer.close();
+					} catch (IOException e) {
+						Log.e(LOG_TAG, "IOException while closing file", e);
+					}
+			    }
+			}    	
 		}
+	}
+	
+    public void loadACItemsFromFile() {
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(new File(getFilesDir(), ACCustomTipPercentFile)));
+			String item;
+			Log.d(LOG_TAG, "Reading tip percent from file");
+			while ((item = reader.readLine()) != null) {
+			    if (!item.isEmpty()) {
+			    	Log.d(LOG_TAG, "Item:" + item);
+			    	acCustomTipPercentSet.add(item);
+			    }
+			}
+			acCustomTipPercentItems = new ArrayList<String>(acCustomTipPercentSet);
+		} catch (FileNotFoundException e) {
+			Log.e(LOG_TAG, "File does not exist:" + ACCustomTipPercentFile + " in parent path " + getFilesDir());
+			return;
+		} catch (IOException e) {
+			Log.e(LOG_TAG, "IOException while reading file", e);
+			return;
+		} finally {
+		    if (reader != null) {
+		    	try {
+					reader.close();
+				} catch (IOException e) {
+					Log.e(LOG_TAG, "IOException while closing file", e);
+				}
+		    }
+		}
+	}
+    
+    @Override
+    protected void onPause() {
+    	super.onPause();
+    	saveACItemsToFile();
+    }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	loadACItemsFromFile();
+    	acCustomTipPercentAdapter.notifyDataSetChanged();
+    }
 }
