41d63f111221bf7e20e3789711b14a3ea50434de
Richa Khandelwal <kricha@lab126.com>
2014-06-10 22:37:59 -0700

    More features, fixes and README

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ TipCalculator
 =============
 
 Codepath assignment  - Tip Calculator app
+
+This is an app to calculate tip based on amount billed. 
+User can enter the amount billed and tip percent user wants to pay, and the app shows
+the total amount the user will need to pay and the tip amount.
+Additionally the user can split the amount with his peers by specifying how many
+ways to split it and the app will display the total amount per person.
+
+Time Spent : 20 hrs
+
+* [x] Required : User is displayed the tip of specified percentage for specified entered amount
+* [x] Required : User enters the total amount of the transaction
+* [x] Required : User can select between tip amounts (i.e 10%, 15%, 20%)
+* [x] Required : Upon selecting tip amount, formatted tip value is displayed
+* [x] Optional : User changes the total amount and updated tip is reflected automatically
+* [x] Optional : User can select custom tip percentage if desired
+* [x] Optional : User can select how many ways to split the tip
+* [x] Optional : User can edit preset tip percentages and have them persist across launches - AutocompleteTextView
+* [x] Optional : Experiment with trying input widgets to replace the buttons and/or textviews - Used custom view to increment/decrement num ways to split.
+* [x] Optional : Improve the user interface and experience by using images and/or colors - Background image, font colors and shadows, Theme and custom styles.
+
+Walkthrough of all user stories:
+
+![Video Walkthrough](anim_tip_calculator.gif)
--- src/org/codepath/assignment1/tipcalculator/TipCalculator.java
+++ src/org/codepath/assignment1/tipcalculator/TipCalculator.java
@@ -13,38 +13,60 @@ import java.util.List;
 import java.util.Set;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnFocusChangeListener;
+import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
+import android.widget.Toast;
 
 public class TipCalculator extends Activity {
 	private static final String LOG_TAG = TipCalculator.class.getSimpleName();
 	private static final int DEFAULT_NUM_PEOPLE = 1;
-	private static final String ACCustomTipPercentFile = "acCustomTipPercent";
+	private static final String AC_CUSTOM_TIP_PERCENT_FILE = "acCustomTipPercent";
+	private static final long REPEAT_DELAY = 50;
+	private static final String OTHER_STRING = "Other";
+	private static final String TEN_PERCENT_STRING = "10%";
+	private static final String FIFTEEN_PERCENT_STRING = "15%";
+	private static final String TEN_STRING = "10";
+	private static final String FIFTEEN_STRING = "15";
 	
-	private EditText etBaseAmount;
+	private EditText mEtBaseAmount;
 	private RadioGroup rgTipPercent;
 	private TextView tvTotalAmount;
 	private TextView tvTipAmount;
-	private TextView tvBaseAmountHelper;
 	private AutoCompleteTextView acCustomTipPercent;
 	private EditText etNumPeople;
 	private TextView tvTipPerPerson;
 	private List<String> acCustomTipPercentItems;
 	private Set<String> acCustomTipPercentSet;
 	private ArrayAdapter<String> acCustomTipPercentAdapter;
+	private Button bNumPersonsMore;
+	private Button bNumPersonsLess;
+	
+	private int mNumPeople = DEFAULT_NUM_PEOPLE;
+    private Handler mRepeatUpdateHandler = new Handler();
+	
+	private boolean mAutoIncrement = false;
+	private boolean mAutoDecrement = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -57,50 +79,45 @@ public class TipCalculator extends Activity {
 	            getTipPercentAndDisplayTip();
 	        }
 	    });
+		
 		acCustomTipPercent = (AutoCompleteTextView) findViewById(R.id.autoCustomPercent);
+		acCustomTipPercent.setTextColor(Color.BLACK);
 		acCustomTipPercentItems = new ArrayList<String>();
 		acCustomTipPercentSet = new HashSet<String>();
 		loadACItemsFromFile();
 		acCustomTipPercentAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, acCustomTipPercentItems);
 		acCustomTipPercent.setAdapter(acCustomTipPercentAdapter);
+		acCustomTipPercent.setOnFocusChangeListener(new OnFocusChangeListener() {
+			
+			@Override
+			public void onFocusChange(View v, boolean hasFocus) {
+				if (hasFocus) {
+				    RadioButton customPercentRb = (RadioButton) findViewById(R.id.rbCustomPercent);
+				    customPercentRb.setChecked(true);
+				}
+			}
+		});
 		acCustomTipPercent.setOnEditorActionListener(new OnEditorActionListener() {
 			@Override
 			public boolean onEditorAction(TextView v, int actionId,
 					KeyEvent event) {
 				Log.d(LOG_TAG, "onEditorAction. Custom Percent");
 				hideSoftKeyboard(acCustomTipPercent);
-				int selectedTipPercentId = rgTipPercent.getCheckedRadioButtonId();
-				RadioButton selectedRb = (RadioButton) findViewById(selectedTipPercentId);
-				if (selectedRb != null && "other".equals(selectedRb.getText())) {
-					String tipPercentString = acCustomTipPercent.getText().toString();
-					if (!acCustomTipPercentSet.contains(tipPercentString)) {
-						acCustomTipPercentItems.add(tipPercentString);
-						acCustomTipPercentSet.add(tipPercentString);
-						acCustomTipPercentAdapter.notifyDataSetChanged();
-					}
-					try {
-					    double tipPercent = Double.parseDouble(tipPercentString)/100;
-					    if (tipPercent <= 0) {
-			            	displayBillAmountError("Custom tip percent cannot be negative");
-			            	return true;
-			            }
-					    computeAndDisplayTip(tipPercent);
-					} catch (NumberFormatException nfe) {
-						displayBillAmountError("Custom Percent Entered is not a number");
-					}
-				} 
+				getTipPercentAndDisplayTip();
 				return true;
 			}
 		});
-		etBaseAmount = (EditText) findViewById(R.id.etBillAmount);
-		etBaseAmount.setOnEditorActionListener(new OnEditorActionListener() {
+		
+		mEtBaseAmount = (EditText) findViewById(R.id.etBillAmount);
+		mEtBaseAmount.setTextColor(Color.BLACK);
+		mEtBaseAmount.setOnEditorActionListener(new OnEditorActionListener() {
 			@Override
 			public boolean onEditorAction(TextView v, int actionId,
 					KeyEvent event) {
-				hideSoftKeyboard(etBaseAmount);
+				hideSoftKeyboard(mEtBaseAmount);
 				Log.d(LOG_TAG, "onEditorAction. actionId:" + actionId + " , IME_ACTION_DONE=" + EditorInfo.IME_ACTION_DONE);
 				try {
-					double billAmount = Double.parseDouble(etBaseAmount.getText().toString());
+					double billAmount = Double.parseDouble(mEtBaseAmount.getText().toString());
 					if (billAmount < 0) {
 						displayBillAmountError("Bill Amount cannot be less than zero");	
 						return true;
@@ -113,22 +130,90 @@ public class TipCalculator extends Activity {
 				return true;
 			}
 		});
+		
 		etNumPeople = (EditText) findViewById(R.id.etNumPeople);
+		etNumPeople.setTextColor(Color.BLACK);
 		etNumPeople.setOnEditorActionListener(new OnEditorActionListener() {
 
 			@Override
 			public boolean onEditorAction(TextView v, int actionId,
 					KeyEvent event) {
 				hideSoftKeyboard(etNumPeople);
-				getNumPeopleSelected();
-				getTipPercentAndDisplayTip();
+				int numPeople = getNumPeopleSelected();
+				if (numPeople <= 0) {
+					displayBillAmountError("Number of people cannot be zero");
+					etNumPeople.setText(String.valueOf(mNumPeople));
+				} else {
+					getTipPercentAndDisplayTip();	
+				}
 				return true;
 			}
 	    });
-		tvBaseAmountHelper = (TextView) findViewById(R.id.tvBaseAmountHelper);
+		
 		tvTipAmount = (TextView) findViewById(R.id.tvTipAmount);
+		
 		tvTipPerPerson = (TextView) findViewById(R.id.tvTipAmountPerPerson);
+		
 		tvTotalAmount = (TextView) findViewById(R.id.tvTotalBillAmount);
+		
+		bNumPersonsLess = (Button) findViewById(R.id.buttonMinus);
+		bNumPersonsLess.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				decrementNumPersons();
+			}
+		});
+		
+		// Auto Decrement for a long click
+		bNumPersonsLess.setOnLongClickListener( 
+				new View.OnLongClickListener(){
+					public boolean onLongClick(View arg0) {
+						mAutoDecrement = true;
+						mRepeatUpdateHandler.post( new RepetetiveUpdater() );
+						return false;
+					}
+	    });
+
+		// When the button is released, if we're auto decrementing, stop
+		bNumPersonsLess.setOnTouchListener( new View.OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				if( event.getAction() == MotionEvent.ACTION_UP && mAutoDecrement ){
+					mAutoDecrement = false;
+				}
+				return false;
+			}
+		});
+
+		bNumPersonsMore = (Button) findViewById(R.id.buttonPlus);
+		bNumPersonsMore.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				incrementNumPersons();
+			}
+		});
+		// Auto increment for a long click
+		bNumPersonsMore.setOnLongClickListener( 
+						new View.OnLongClickListener(){
+							public boolean onLongClick(View arg0) {
+								mAutoIncrement = true;
+								mRepeatUpdateHandler.post( new RepetetiveUpdater() );
+								return false;
+							}
+						}
+				);
+				
+	    // When the button is released, if we're auto incrementing, stop
+		bNumPersonsMore.setOnTouchListener( new View.OnTouchListener() {
+					@Override
+					public boolean onTouch(View v, MotionEvent event) {
+						if( event.getAction() == MotionEvent.ACTION_UP && mAutoIncrement ){
+							mAutoIncrement = false;
+						}
+						return false;
+					}
+	    });
 	}
 	
     public void getTipPercentAndDisplayTip() {
@@ -138,9 +223,14 @@ public class TipCalculator extends Activity {
 		if (selectedRb != null) {
 			String rbText = selectedRb.getText().toString();
 			String tipPercentString = "0";
-			if ("other".equals(rbText)) {
+			if (OTHER_STRING.equals(rbText)) {
 				tipPercentString = acCustomTipPercent.getText().toString();
 				if (tipPercentString != null && !tipPercentString.isEmpty()) {
+					if (!acCustomTipPercentSet.contains(tipPercentString)) {
+						acCustomTipPercentItems.add(tipPercentString);
+						acCustomTipPercentSet.add(tipPercentString);
+						acCustomTipPercentAdapter.notifyDataSetChanged();
+					}
 					try {
 						tipPercent = Double.parseDouble(tipPercentString)/100;
 						if (tipPercent <= 0) {
@@ -155,12 +245,10 @@ public class TipCalculator extends Activity {
 					computeAndDisplayTip(tipPercent);	
 				}
 			} else {
-				if ("10%".equals(rbText)) {
-				    tipPercentString = "10";
-				} else if ("20%".equals(rbText)) {
-				    tipPercentString = "20";
-				} else if ("15%".equals(rbText)) {
-				    tipPercentString = "15";
+				if (TEN_PERCENT_STRING.equals(rbText)) {
+				    tipPercentString = TEN_STRING;
+				} else if (FIFTEEN_PERCENT_STRING.equals(rbText)) {
+				    tipPercentString = FIFTEEN_STRING;
 				} else {
 				    // not valid	
 				}
@@ -178,19 +266,18 @@ public class TipCalculator extends Activity {
 	
 	public void computeAndDisplayTip(double tipPercent) {
 		try {
-		    double billAmount = Double.parseDouble(etBaseAmount.getText().toString());
+		    double billAmount = Math.round(Double.parseDouble(mEtBaseAmount.getText().toString()) * 100)/100;
 		    if (billAmount < 0) {
 		        displayBillAmountError("Bill Amount cannot be less than zero");	
 		        return;
-		    } else {
-		    	tvBaseAmountHelper.setText("");
-		    }
+		    } 
 		    double tipAmount = (double)Math.round((billAmount * tipPercent) * 100) / 100;
-		    tvTipAmount.setText("$" + String.valueOf(tipAmount));
+		    tvTipAmount.setText("$ " + String.valueOf(tipAmount));
 		    int numPeople = getNumPeopleSelected();
 		    double totalAmount = billAmount + tipAmount;
-		    tvTipPerPerson.setText(String.valueOf(totalAmount/numPeople));
-		    tvTotalAmount.setText(String.valueOf(totalAmount));
+		    double billPerPerson = Math.round((totalAmount/numPeople) * 100)/100;
+		    tvTipPerPerson.setText("$ " + String.valueOf(billPerPerson));
+		    tvTotalAmount.setText("$ " + String.valueOf(totalAmount));
 		} catch (NumberFormatException nfe) {
 			Log.d(LOG_TAG, "Bill Amount Invalid");
 			displayBillAmountError("Bill Amount must be a number");
@@ -199,8 +286,9 @@ public class TipCalculator extends Activity {
 	
 	public void displayBillAmountError(String errorString) {
 		Log.e(LOG_TAG, errorString);
-	    tvBaseAmountHelper.setText(errorString);	
-	    tvBaseAmountHelper.setTextColor(Color.RED);
+		Toast toast = Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT);
+		toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+		toast.show();
 	}
 	
 	public int getNumPeopleSelected() {
@@ -209,9 +297,6 @@ public class TipCalculator extends Activity {
 	    if (numPeopleString != null && !numPeopleString.isEmpty()) {
 	    	try {
 	            numPeople = Integer.parseInt(etNumPeople.getText().toString());
-	            if (numPeople <= 0) {
-	            	displayBillAmountError("Number of People cannot be less than 1");
-	            }
 	    	} catch(NumberFormatException nfe) {
 	    		Log.e(LOG_TAG, "Invalid number of people");
 	    		displayBillAmountError("Number of people entered should be a number");
@@ -229,7 +314,7 @@ public class TipCalculator extends Activity {
 		if (acCustomTipPercentSet != null && !acCustomTipPercentSet.isEmpty()) {
 			BufferedWriter writer = null;
 		    try {
-				writer = new BufferedWriter(new FileWriter(new File(getFilesDir(), ACCustomTipPercentFile)));
+				writer = new BufferedWriter(new FileWriter(new File(getFilesDir(), AC_CUSTOM_TIP_PERCENT_FILE)));
 				Log.d(LOG_TAG, "Saving tip percent to file");
 				for (String item : acCustomTipPercentSet) {
 					Log.d(LOG_TAG, "Item:" + item);
@@ -237,7 +322,7 @@ public class TipCalculator extends Activity {
 					writer.newLine();
 				}
 			} catch (FileNotFoundException e) {
-				Log.e(LOG_TAG, "File does not exist:" + ACCustomTipPercentFile + " in parent path " + getFilesDir());
+				Log.e(LOG_TAG, "File does not exist:" + AC_CUSTOM_TIP_PERCENT_FILE + " in parent path " + getFilesDir());
 				return;
 			} catch (IOException e) {
 				Log.e(LOG_TAG, "IOException while writing to file", e);
@@ -257,7 +342,7 @@ public class TipCalculator extends Activity {
     public void loadACItemsFromFile() {
 		BufferedReader reader = null;
 		try {
-			reader = new BufferedReader(new FileReader(new File(getFilesDir(), ACCustomTipPercentFile)));
+			reader = new BufferedReader(new FileReader(new File(getFilesDir(), AC_CUSTOM_TIP_PERCENT_FILE)));
 			String item;
 			Log.d(LOG_TAG, "Reading tip percent from file");
 			while ((item = reader.readLine()) != null) {
@@ -268,7 +353,7 @@ public class TipCalculator extends Activity {
 			}
 			acCustomTipPercentItems = new ArrayList<String>(acCustomTipPercentSet);
 		} catch (FileNotFoundException e) {
-			Log.e(LOG_TAG, "File does not exist:" + ACCustomTipPercentFile + " in parent path " + getFilesDir());
+			Log.e(LOG_TAG, "File does not exist:" + AC_CUSTOM_TIP_PERCENT_FILE + " in parent path " + getFilesDir());
 			return;
 		} catch (IOException e) {
 			Log.e(LOG_TAG, "IOException while reading file", e);
@@ -296,4 +381,41 @@ public class TipCalculator extends Activity {
     	loadACItemsFromFile();
     	acCustomTipPercentAdapter.notifyDataSetChanged();
     }
+    
+    public void incrementNumPersons(){
+    	mNumPeople = mNumPeople + 1;
+    	etNumPeople.setText( String.valueOf(mNumPeople));
+    	getTipPercentAndDisplayTip();
+    	
+    }
+
+	public void decrementNumPersons(){
+		if (mNumPeople > 1) {
+    	    mNumPeople = mNumPeople - 1;
+    	    etNumPeople.setText( String.valueOf(mNumPeople));
+    	    getTipPercentAndDisplayTip();
+		}
+	}
+	
+	class RepetetiveUpdater implements Runnable {
+		public void run() {
+			if(mAutoIncrement ){
+				incrementNumPersons();
+				mRepeatUpdateHandler.postDelayed(new RepetetiveUpdater(), REPEAT_DELAY);
+			} else if(mAutoDecrement ){
+				decrementNumPersons();
+				mRepeatUpdateHandler.postDelayed(new RepetetiveUpdater(), REPEAT_DELAY);
+			}
+		}
+	}
+	
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		Log.e(LOG_TAG, "Configuration Changed");
+	    if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+	    	TableLayout result = (TableLayout) findViewById(R.id.tlTipResult);
+	    	result.setVisibility(ViewGroup.INVISIBLE);
+	    }
+	}
 }
